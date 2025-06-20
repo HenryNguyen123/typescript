f74e0c6d93a488cd5896707d45ca037a7e955e3e
Minh Nhat <nhokkudo143@gmail.com>
2025-06-20 21:57:06 +0700

    overloading

---
+++ control_flow_statetament/overLoading.ts
@@ -0,0 +1,8 @@
+function addNew (a: number, b: number): number
+function addNew (a: string, b: string): string
+
+function addNew (a: any, b: any) {
+    return a +b
+}
+
+console.log("checkk >>>> ", addNew(2, 3)," check>>>> ",  addNew("nguyen van a", " 21 tuoi"))
